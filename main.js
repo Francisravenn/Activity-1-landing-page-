3c3991c3198274eba575bcdc0b066cd37164a50d
Francisravenn <163851041+Francisravenn@users.noreply.github.com>
2024-08-28 21:52:37 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+    
+};
+
+ScrollReveal().reveal(".navigation", {
+    duration: 1000,
+});
+
+// header container
+ScrollReveal().reveal(".vertical-align", {
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".homecontent", {
+    ...scrollRevealOption,
+    origin: 'left',
+});
+
+ScrollReveal().reveal(".homepic", {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".abocon1", {
+    ...scrollRevealOption,
+    origin: 'left',
+});
+
+ScrollReveal().reveal(".abopic1", {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".abopic2", {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".abocon2", {
+    ...scrollRevealOption,
+    origin: 'left',
+});
+
+ScrollReveal().reveal(".abocon3", {
+    ...scrollRevealOption,
+    origin: 'left',
+});
+
+ScrollReveal().reveal(".abopic3", {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".abopic4", {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal(".abocon4", {
+    ...scrollRevealOption,
+    origin: 'left',
+});
